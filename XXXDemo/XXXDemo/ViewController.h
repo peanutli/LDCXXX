33b13cc5e6828a9f045f01bacac8e17323040fa7
花生 <bjlidachuan@163.com>
2016-11-13 00:24:55 +0800

    备注

---
+++ XXXDemo/XXXDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXXDemo
+//
+//  Created by 花生 on 16/11/13.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XXXDemo/XXXDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XXXDemo
+//
+//  Created by 花生 on 16/11/13.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
